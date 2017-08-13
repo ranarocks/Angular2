b3a6c73fdc5a52e3dd35825118ccef07882049c8
ranarocks <jrana4@asiapac.globalcsc.net>
2017-08-13 10:39:29 +0530

    changes

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -10,7 +10,7 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
     })
-    .compileComponents();
+    .compileComponents();   //test
   }));
 
   beforeEach(() => {
